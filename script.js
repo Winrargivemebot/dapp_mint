bd5650f530a79c829370874946a1b66eee4d67e4
Winrargivemebot <104037759+Winrargivemebot@users.noreply.github.com>
2023-08-15 18:30:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 // In Node.js use: const Web3 = require('web3');
 
 const web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
+
+web3.eth.sendTransaction({from: '0x123...', data: '0x432...'})
+.once('sending', function(payload){ ... })
+.once('sent', function(payload){ ... })
+.once('transactionHash', function(hash){ ... })
+.once('receipt', function(receipt){ ... })
+.on('confirmation', function(confNumber, receipt, latestBlockHash){ ... })
+.on('error', function(error){ ... })
+.then(function(receipt){
+    // will be fired once the receipt is mined
+});
+
